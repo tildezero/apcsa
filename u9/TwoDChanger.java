90b37cb32370c04d2cbdd150a54fc5092c4760f1
tildezero <suhasvoolla@gmail.com>
2022-03-23 15:12:20 +0000

    bruh moment

--- u9/TwoDChanger.java
+++ u9/TwoDChanger.java
@@ -2,6 +2,15 @@ package u9;
 
 public class TwoDChanger {
     public static void main(String[] args) {
-        
+        int ro = (int)(Math.random() * 11 + 10);
+        int co = (int)(Math.random() * 11 + 10);    
+
+        int[][] m = new int[ro][co];
+
+        for (int r = 0; r < m.length; r++) {
+            for (int c = 0; c < m[0].length; c++) {
+                m[r][c] = (int)(Math.random() * 100);
+            }
+        }
     }
 }
