311bf9566b6d6290fab3ba914128ffa75818a96b
tildezero <suhasvoolla@gmail.com>
2022-04-23 16:51:35 +0000

    u11 labs complete

---
+++ SSN.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SSN {
+   public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       System.out.print("Enter an SSN: ");
+       String ssn = s.nextLine();
+       System.out.println(ssn.matches("\\d{4}-\\d{2}-\\d{3}") ? "Valid SSN" : "Invalid SSN");
+   } 
+}
